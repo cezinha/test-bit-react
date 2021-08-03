beb1c8e2ed244f51702bc08f744851faeb47a250
Celina Uemura <ext-c.uemura@rappi.com>
2021-08-03 16:33:16 -0300

    - added bit component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { TransactionLine } from "@cezinha/mini-app-components.ui.transaction-line";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
+      <div style={{ margin: "16px", maxWidth: "798px" }}>
+        <TransactionLine
+          date={new Date()}
+          title='Título da transação'
+          description='Pagamento de boleto'
+          type='debit'
+          value={99.99}
+          tag='service'
+          showDay={true}
+        />
+      </div>
     </div>
   );
 }
